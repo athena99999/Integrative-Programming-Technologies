7cee395182d2717ae5c1a0ca15d18bdfc4749fdf
athena99999 <160150347+athena99999@users.noreply.github.com>
2024-02-19 18:50:10 +0800

    Create index.php

---
+++ 0_REGISTER/index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<form action="process.php" method="POST" >
+
+name: <input type="text" name="name"><br><br>
+username: <input type="text" name="username"><br><br>
+firstname: <input type="text" name="firstname"><br><br>
+middlename: <input type="text" name="middlename"><br><br>
+lastname: <input type="text" name="lastname"><br><br>
+role: <input type="text" name="role"><br><br>
+password: <input type="password" name="password"><br><br>
+
+<button type="submit" name = "regButton">Button</button>
+</form>
+
+</body>
+</html>
