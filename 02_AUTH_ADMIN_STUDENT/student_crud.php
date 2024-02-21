835e21efb4ff5e8e4091546e696c3cffaa9f7216
athena99999 <160150347+athena99999@users.noreply.github.com>
2024-02-21 15:09:16 +0800

    Create student_crud.php

---
+++ 02_AUTH_ADMIN_STUDENT/student_crud.php
@@ -0,0 +1,14 @@
+<?php
+session_start(); // Start the session
+
+if(isset($_SESSION['firstname']) && isset($_SESSION['role'])){
+    $firstname = $_SESSION['firstname'];
+    $role = $_SESSION['role'];
+    // Display the firstname and role
+    echo "Welcome, $firstname! You are logged in as a student.";
+} else {
+    // Redirect if the session variables are not set
+    header("Location: login.php");
+    exit();
+}
+?>
